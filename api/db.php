927c52b3023d02b79092b68ecbcbf770205e89ed
Mritunjay Pramanick <103257250+mritunjaypramanick@users.noreply.github.com>
2026-02-13 22:09:41 +0530

    Delete api directory

--- api/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "db_portfolio";
-
-    $conn = mysqli_connect($host,$username,$password,$dbname) or die("Connection Failed ! ");
-
-?>
